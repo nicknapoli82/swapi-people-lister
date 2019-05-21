425b172af9272820cb376d689429eb4c3c4196ba
Nicholas Napoli <nicknapoli82@gmail.com>
2019-05-20 17:14:56 -0700

    Added sort function for named list. A little more Style

--- test.js
+++ test.js
@@ -11,12 +11,14 @@ document.getElementById("list").addEventListener("mouseover", (e) => {
 
 document.getElementById("list").addEventListener("mouseout", (e) => {
     if (e.target.nodeName === 'LI') {
-	e.target.style.backgroundColor = "white";
+	e.target.style.backgroundColor = "goldenrod";
 	if (e.target === lastSelected) {
 	    e.target.style.backgroundColor = "lightblue";
 	}
     }
 });
+
+document.getElementById("sort-by").addEventListener("change", sortListedNames);
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Above is really just the raw order of things being called. Below is the meat and potatos //
@@ -37,18 +39,17 @@ async function fetchPeople() {
 	return null;
     };
 
-    let response = await fetch(url);
+    let response = await fetch(url, { cache: 'force-cache' });
     response = await response.json();
     response.results.map((d) => data.push(d));
     populateNames(data);
 
-    console.log(response.count / data.length);
     for (let i = 2, end = Math.ceil(response.count / data.length); i <= end; i++) {
 	additionalFetch(i);
     }
 
     async function additionalFetch(index) {
-	let response = await fetch(url + `?page=${index}`);
+	let response = await fetch(url + `?page=${index}`, { cache: 'force-cache' });
 	response = await response.json();
 	response.results.map((d) => data.push(d));
 	populateNames(data);	
@@ -61,7 +62,7 @@ function populateNames(nL) {
     //Using <ul>
     //Each name in nL will have a <li>
     const list = nL.map((n) => {
-	return `<li data-listname="${n.name}">${n.name}</li>`;
+	return `<li id="${n.name}">${n.name}</li>`;
     }).join('');
     document.getElementById("list").innerHTML = `<ul>${list}</ul>`;
 }
@@ -71,7 +72,7 @@ function toNamedValue(e) {
     if (e.target.nodeName !== 'LI') return;
     let result = "";
     const keysAvailable = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender'];
-    const named = getNamed(e.target.innerText);
+    const named = getNamed(e.target.id);
     
     for (let k in named) {
 	if (keysAvailable.includes(k)) {
@@ -80,10 +81,59 @@ function toNamedValue(e) {
     }
 
     if (lastSelected !== undefined) {
-	lastSelected.style.backgroundColor = "white";
+	lastSelected.style.backgroundColor = "goldenrod";
     }
     e.target.style.backgroundColor = "lightblue";
     lastSelected = e.target;
 
     document.getElementById("named-value").innerHTML = result;
+}
+
+function sortListedNames(e) {
+    const filter = e.target.value;
+    console.log(filter);
+    const nameList = Array.from(document.getElementById("list").getElementsByTagName('LI')).map((l) => {
+	return l.id;
+    });
+
+    let filterList = nameList.map((n) => getNamed(n))
+	.sort((a, b) => {
+	    if (a[filter] < b[filter]) return -1;
+	    else if (a[filter] === b[filter]) return 0;
+	    else return 1;
+	});
+
+    // If the value of sort method is 'none' or 'name' just use
+    // the populateNames() function
+    if (filter === 'none' || filter === 'name') {
+	populateNames(filterList);
+	return;
+    }
+    
+    // Get a collection of list items to dump into an UL
+    let list = filterList.map((n) => {
+	return `<li id="${n.name}">${n.name}</li>`;
+    });
+
+    console.log(list);
+    let counter = 0;
+    let currentList = '';
+    let testList = '';
+    let ulList = document.getElementById("list");
+    ulList.innerHTML = '';
+    while (counter < list.length) {
+	let ul = document.createElement('UL');
+	ul.innerHTML = filterList[counter][filter];
+	currentList = filterList[counter][filter];
+//	console.log(currentList);
+	while ( counter < list.length && currentList === filterList[counter][filter]) {
+	    ul.innerHTML += list[counter];
+	    currentList = filterList[counter][filter];
+	    counter++;
+	}
+
+	ulList.appendChild(ul);
+    }
+    
+//    document.getElementById("list").innerHTML = `<ul>${list}</ul>`;    
 }
